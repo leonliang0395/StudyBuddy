6578a3f084e1371e58f967f3605e8bad0c1153ba
Leon Liang <leonliang0395@gmail.com>
2015-09-12 01:14:17 -0400

    first flask app

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/about")
+def hello():
+    return "Hello World!"
+
+@app.route("/poop")
+def poop():
+	return "<h1> poop </h1><h2> poop again </h2>"
+
+if __name__ == "__main__":
+    app.run()
